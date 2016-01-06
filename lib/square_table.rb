3238114b6aacf10a8fd293e6fd1d529073487635
Lynn Chin <lynchin@gmail.com>
2016-01-06 11:32:45 +1100

    Fix up code formatting issues in square_table and its spec class

--- lib/square_table.rb
+++ lib/square_table.rb
@@ -5,8 +5,8 @@ class SquareTable
   end
 
   def check_position(x_coordinate, y_coordinate)
-    if (x_coordinate < 0 or x_coordinate > rows) or
-        (y_coordinate < 0 or y_coordinate > columns)
+    if (x_coordinate < 0 || x_coordinate > rows) ||
+        (y_coordinate < 0 || y_coordinate > columns)
       false
     else
       true
--- spec/square_table_spec.rb
+++ spec/square_table_spec.rb
@@ -2,11 +2,10 @@ require 'spec_helper'
 require_relative '../lib/square_table'
 
 describe 'SquareTable' do
-
-  let(:table) { SquareTable.new(5,5) }
+  let(:table) { SquareTable.new(5, 5) }
 
   describe '#new' do
-    it 'should create an instance of SquareTable' do
+    it 'create an instance of SquareTable' do
       expect(table).to be_a SquareTable
     end
   end
@@ -21,12 +20,11 @@ describe 'SquareTable' do
 
   describe '#check_position' do
     context 'when input position is outside the table range' do
-       it { expect(table.check_position(-1,1)).to eq false }
+      it { expect(table.check_position(-1, 1)).to eq false }
     end
 
     context 'when input position is within the table range' do
-      it { expect(table.check_position(1,1)).to eq true }
+      it { expect(table.check_position(1, 1)).to eq true }
     end
   end
-
 end
