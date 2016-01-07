2aeb1568be0f287d15011e3c008a753cc94b8e24
Lynn Chin <lynchin@gmail.com>
2016-01-07 23:59:57 +1100

    Update robot_simulator as a ruby script file.

--- robot_simulator.rb
+++ robot_simulator.rb
@@ -1,2 +1,15 @@
-require './lib/main'
-Main.new(ARGV.dup)
+#!/usr/bin/env ruby
+
+require_relative 'lib/simulator'
+
+if ARGV.length > 0
+  puts 'No argument is required. Command to run: ruby robot_simulator.rb'
+  exit 1
+end
+
+simulator = Simulator.new
+
+while command_line = STDIN.gets
+  output = simulator.execute(command_line)
+  puts output if command_line.chomp() == 'REPORT'
+end
