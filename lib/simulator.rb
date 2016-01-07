942eaaaeb10639c2386ef88f9809af809cbe532e
Lynn Chin <lynchin@gmail.com>
2016-01-07 23:59:15 +1100

    Add simulator and spec class that gathers everything in one place

---
+++ lib/simulator.rb
@@ -0,0 +1,93 @@
+require_relative '../lib/robot'
+require_relative '../lib/square_table'
+require_relative '../lib/position'
+
+class Simulator
+  attr_accessor :table, :robot
+
+  def initialize(width = 5, height = 5)
+    @table = SquareTable.new(width, height)
+    @robot = Robot.new
+  end
+
+  def execute(line)
+    return nil if line.strip.empty?
+    tokens = line.split(/\s+/)
+    operator = tokens.first
+    arguments = tokens.last
+
+    execute_command(operator, arguments)
+  end
+
+  def execute_command(operator, arguments)
+    case operator
+    when 'PLACE'
+      place(arguments)
+    when 'REPORT'
+      report
+    when 'MOVE'
+      move
+    when 'LEFT'
+      left
+    when 'RIGHT'
+      right
+    else
+      'Invalid command line agrument #{command}'
+    end
+  end
+
+  def place(arguments)
+    tokens = arguments.split(/,/)
+    x = tokens[0].to_i
+    y = tokens[1].to_i
+    orientation = tokens[2].downcase.to_sym
+    place_robot(x, y, orientation)
+    nil
+  end
+
+  def report
+    @table.position_to_s + ',' + @robot.to_s unless @table.position.nil? && @robot.orientation.nil?
+  end
+
+  def left
+    return nil unless @table.placed?
+    @robot.left
+  end
+
+  def right
+    return nil unless @table.placed?
+    @robot.right
+  end
+
+  def move
+    fail 'Robot not found' if @robot.nil?
+
+    case robot.orientation
+    when :north
+      move_position(0, 1, @table)
+    when :south
+      move_position(0, -1, @table)
+    when :east
+      move_position(1, 0, @table)
+    when :west
+      move_position(-1, 0, @table)
+    else
+      fail 'No such move. Invalid orientation'
+    end
+    nil
+  end
+
+  private
+
+  def move_position(x_value, y_value, table)
+    new_position = Position.new(x_value,y_value)
+    fail 'Invalid move in position.' unless table.valid_position(new_position)
+    table.update(new_position)
+  end
+
+  def place_robot(x_coordinate, y_coordinate, orientation)
+    @table.place Position.new(x_coordinate,y_coordinate)
+    fail 'Fail to place robot. Invalid position.' unless @table.placed?
+    @robot.set_orientation orientation
+  end
+end
---
+++ spec/simulator_spec.rb
@@ -0,0 +1,191 @@
+require 'rspec'
+require_relative '../lib/robot'
+require_relative '../lib/square_table'
+require_relative '../lib/simulator'
+
+describe 'Simulator' do
+
+  let(:simulator) { Simulator.new }
+
+  let(:table) { double 'table' }
+
+  let(:robot) { double 'robot' }
+
+  let(:position) { double 'position' }
+
+  describe '#new' do
+    it 'creates an instance of Simulator' do
+      expect(simulator).to be_a Simulator
+    end
+
+    it 'has a table' do
+      expect(simulator.table).to be_a SquareTable
+    end
+
+    it 'has a robot' do
+      expect(simulator.robot).to be_a Robot
+    end
+  end
+
+  describe '#execute' do
+    context 'with an empty string' do
+      it 'returns nothing' do
+        expect(simulator.execute('')).to eq nil
+      end
+    end
+
+    context 'with whitespaces' do
+      it 'returns nothing' do
+        expect(simulator.execute('   ')).to eq nil
+      end
+    end
+
+    context 'with move command' do
+      describe 'before place command' do
+        it 'raise an exception error' do
+          expect { simulator.execute('MOVE') }.to raise_error RuntimeError
+        end
+      end
+    end
+
+    context 'with left command' do
+      describe 'before place command' do
+        it 'does nothing' do
+          expect(simulator.execute('LEFT')).to eq nil
+        end
+      end
+    end
+
+    context 'with right command' do
+      describe 'before place command' do
+        it 'does nothing' do
+          expect(simulator.execute('RIGHT')).to eq nil
+        end
+      end
+    end
+
+    context 'with report command' do
+      describe 'before place command' do
+        it 'does nothing' do
+          expect(simulator.execute('REPORT')).to eq nil
+        end
+      end
+    end
+
+    context 'if nothing is on table' do
+      it 'places robot on table' do
+        expect(simulator.execute('PLACE 0,0,NORTH')).to eq nil
+      end
+    end
+  end
+
+  describe '#place' do
+    context 'with an invalid coordinate' do
+      it 'raise an exception error' do
+        expect { simulator.place('6,6,NORTH') }.to raise_error RuntimeError
+      end
+    end
+
+    context 'with an invalid direction' do
+      it 'raise an exception error' do
+        expect { simulator.place('0,0,UP') }.to raise_error RuntimeError
+      end
+    end
+
+    context 'with valid coordinates and direction' do
+      it 'places robot on table' do
+        expect(simulator.place('0,0,NORTH')).to eq nil
+      end
+    end
+  end
+
+  describe '#move' do
+    context 'when robot is placed on table' do
+      before do
+        simulator.execute('PLACE 0,0,NORTH')
+      end
+      it 'allows you to move north' do
+        simulator.execute('MOVE')
+        expect(simulator.table.position.x_coordinate).to eq 0
+        expect(simulator.table.position.y_coordinate).to eq 1
+      end
+    end
+  end
+
+  describe '#left' do
+    context 'when robot is placed on table' do
+      before :each do
+        simulator.execute('PLACE 0,0,NORTH')
+      end
+      it 'change the direction to west' do
+        simulator.execute('LEFT')
+        expect(simulator.robot.orientation).to eq :west
+      end
+
+      it 'change the direction to south' do
+        simulator.robot.set_orientation :west
+        simulator.execute('LEFT')
+        expect(simulator.robot.orientation).to eq :south
+      end
+
+      it 'change the direction to east' do
+        simulator.robot.set_orientation :south
+        simulator.execute('LEFT')
+        expect(simulator.robot.orientation).to eq :east
+      end
+
+      it 'change the direction to south' do
+        simulator.robot.set_orientation :east
+        simulator.execute('LEFT')
+        expect(simulator.robot.orientation).to eq :north
+      end
+    end
+  end
+
+  describe '#right' do
+    context 'when robot is placed on table' do
+      before :each do
+        simulator.execute('PLACE 0,0,NORTH')
+      end
+      it 'change the direction to east' do
+        simulator.execute('RIGHT')
+        expect(simulator.robot.orientation).to eq :east
+      end
+
+      it 'change the direction to south' do
+        simulator.robot.set_orientation :east
+        simulator.execute('RIGHT')
+        expect(simulator.robot.orientation).to eq :south
+      end
+
+      it 'change the direction to east' do
+        simulator.robot.set_orientation :south
+        simulator.execute('RIGHT')
+        expect(simulator.robot.orientation).to eq :west
+      end
+
+      it 'change the direction to south' do
+        simulator.robot.set_orientation :west
+        simulator.execute('RIGHT')
+        expect(simulator.robot.orientation).to eq :north
+      end
+    end
+  end
+
+  describe '#report' do
+    context 'when robot is placed on table' do
+      before :each do
+        simulator.execute('PLACE 0,0,NORTH')
+      end
+      it 'display the current position' do
+        expect(simulator.execute('REPORT')).to eq '0,0,NORTH'
+      end
+    end
+
+    context 'when robot is not on table' do
+      it 'displays nothing' do
+        expect(simulator.execute('REPORT')).to be_nil
+      end
+    end
+  end
+end
